45ddc617e6ebc37ff4650f1a8ec25cfc935a00da
zhenyu.yue <hheight@163.com>
2020-09-23 20:16:10 +0800

    demo for volatile

--- src/com/height/concurrent/volatileDemo/practice/ThreadDemo.java
+++ src/com/height/concurrent/volatileDemo/practice/ThreadDemo.java
@@ -5,9 +5,15 @@ import com.height.common.ThreadUtils;
 public class ThreadDemo extends Thread{
     private String changingFace = "我现在很高兴！";
 
+    public String getChangingFace() {
+        return changingFace;
+    }
+
+
     @Override
     public void run() {
         super.run();
-        ThreadUtils.sleep(1000);
+//        ThreadUtils.sleep(10);
+        changingFace = "我现在不开心了！";
     }
 }
--- src/com/height/concurrent/volatileDemo/practice/VolatileDemoOne.java
+++ src/com/height/concurrent/volatileDemo/practice/VolatileDemoOne.java
@@ -1,8 +1,17 @@
 package com.height.concurrent.volatileDemo.practice;
 
+import com.height.common.ThreadUtils;
+
+import static com.height.common.ThreadUtils.sleep;
+
 public class VolatileDemoOne {
 
-
-
-
+    public static void main(String args[]){
+        ThreadDemo threadDemo = new ThreadDemo();
+        threadDemo.start();
+        for (int i = 0 ; i < 20 ; i++){
+            System.out.println(i+""+threadDemo.getChangingFace());
+        }
+        System.out.println("最后一次机会：" + threadDemo.getChangingFace());
+    }
 }

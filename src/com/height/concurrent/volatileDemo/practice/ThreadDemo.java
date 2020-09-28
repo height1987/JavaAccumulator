a01fa4a34da454bd6a34d216c7e99aabb298aa05
zhenyu.yue <hheight@163.com>
2020-09-28 15:52:13 +0800

    修改demo

--- src/com/height/concurrent/volatileDemo/practice/ThreadDemo.java
+++ src/com/height/concurrent/volatileDemo/practice/ThreadDemo.java
@@ -3,17 +3,21 @@ package com.height.concurrent.volatileDemo.practice;
 import com.height.common.ThreadUtils;
 
 public class ThreadDemo extends Thread {
-    private volatile boolean normalFlag = true;
+    private boolean normalFlag = true;
+    private volatile boolean volatileFlg = true;
 
     @Override
     public void run() {
         ThreadUtils.sleep(1000);
         normalFlag = false;
-        while (true){
+        volatileFlg = false;
 
-        }
     }
     public boolean isNormalFlag() {
         return normalFlag;
+    }
+
+    public boolean isVolatileFlg() {
+        return volatileFlg;
     }
 }
--- src/com/height/concurrent/volatileDemo/practice/VolatileDemoOne.java
+++ src/com/height/concurrent/volatileDemo/practice/VolatileDemoOne.java
@@ -6,37 +6,62 @@ import com.height.common.ThreadUtils;
 public class VolatileDemoOne {
 
     public static void main(String args[]) {
-//        methodOne1();
-//        methodTwo();
-        methodThree();
+//        normalMethodOne();
+//        normalMethodTwo();
+//        normalMethodThree();
+        volatileMethodOne();
     }
-    private static void methodOne() {
+
+
+    /**
+     * 结论：不能正常结束运行
+     * 因为flag在子线程中的工作内存中被更新，
+     * 没有同步到主内存，导致主线程没有获得。
+     * 因为在死循环中，CPU一直被占用，没有触发JVM被动可见变量的操作。
+     */
+    private static void normalMethodOne() {
         ThreadDemo threadDemo = new ThreadDemo();
         threadDemo.start();
         while (threadDemo.isNormalFlag()){
         }
     }
-    private static void methodOne1() {
-        ThreadDemo threadDemo = new ThreadDemo();
-        threadDemo.start();
-        while (true){
-            if(!threadDemo.isNormalFlag()){
-                System.out.println("sss");
-            }
-        }
-    }
-    private static void methodTwo() {
+
+    /**
+     * 结论：可以正常结束运行
+     * 因为flag在子线程中更新，同时System.out.print中有synchronized关键词，
+     * 在获得锁的时候，会把变量全部同步到主内存中。
+     * 这种情况下，只要在while循环中添加synchronized模块，也能达到相同的目的
+     */
+    private static void normalMethodTwo() {
         ThreadDemo threadDemo = new ThreadDemo();
         threadDemo.start();
         while (threadDemo.isNormalFlag()){
             System.out.println("跳出循环!");
         }
     }
-    private static void methodThree() {
+
+    /**
+     * 结论：可以正常结束运行
+     * 因为在死循环中，子线程释放CPU，触发了JVM的被动可见变量的逻辑
+     */
+    private static void normalMethodThree() {
         ThreadDemo threadDemo = new ThreadDemo();
         threadDemo.start();
         while (threadDemo.isNormalFlag()){
             ThreadUtils.sleep(200);
+        }
+    }
+
+    /**
+     * 结论：可以正常结束
+     * 因为flag在子线程中的工作内存中被更新，
+     * 由于变量被volatile关键词修饰，最新值被同步到主内存，
+     * 并且通知其他线程重新获取最新值
+     */
+    private static void volatileMethodOne() {
+        ThreadDemo threadDemo = new ThreadDemo();
+        threadDemo.start();
+        while (threadDemo.isVolatileFlg()){
         }
     }
 }

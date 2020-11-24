8c9fb1c212dcff6f2c932c2e88e1948ace5a5bcd
zhenyu.yue <hheight@163.com>
2020-11-24 23:41:09 +0800

    add lock

--- src/com/height/redisDemo/DistributLock.java
+++ src/com/height/redisDemo/DistributLock.java
@@ -1,5 +1,8 @@
 package com.height.redisDemo;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+
 /**
  * 分布式锁 redis版本
  * 1.唯一性
@@ -8,4 +11,8 @@ package com.height.redisDemo;
  * 4.redis异常时能释放锁
  */
 public class DistributLock {
+    public static void main(String args[]){
+        RedissonClient redissonClient = Redisson.create();
+        redissonClient.getLock("12").lock();
+    }
 }
